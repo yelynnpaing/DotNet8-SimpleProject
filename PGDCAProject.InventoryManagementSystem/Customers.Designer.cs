9ec6b8522a245ba4f2648747cd756af992552ebd
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2024-12-04 20:52:59 +0630

    Add - Customer UI Update and CRUD

--- PGDCAProject.InventoryManagementSystem/Customers.Designer.cs
+++ PGDCAProject.InventoryManagementSystem/Customers.Designer.cs
@@ -30,28 +30,29 @@
         {
             label8 = new Label();
             label14 = new Label();
-            dgItemList = new DataGridView();
-            CusId = new DataGridViewTextBoxColumn();
-            CusName = new DataGridViewTextBoxColumn();
-            Gender = new DataGridViewTextBoxColumn();
-            CusPhone = new DataGridViewTextBoxColumn();
+            dgCustomerList = new DataGridView();
             DeleteBtn = new Button();
             SaveBtn = new Button();
-            EditBtn = new Button();
-            cboCategory = new ComboBox();
+            UpdateBtn = new Button();
+            cboGender = new ComboBox();
             label11 = new Label();
             label10 = new Label();
             label9 = new Label();
-            txtPrice = new MaskedTextBox();
-            txtItem = new MaskedTextBox();
-            ((System.ComponentModel.ISupportInitialize)dgItemList).BeginInit();
+            txtCusPhone = new MaskedTextBox();
+            txtCusName = new MaskedTextBox();
+            NewBtn = new Button();
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            txtCusID = new MaskedTextBox();
+            ((System.ComponentModel.ISupportInitialize)dgCustomerList).BeginInit();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // label8
             // 
             label8.AutoSize = true;
             label8.Font = new Font("Microsoft Sans Serif", 12F);
-            label8.Location = new Point(297, 68);
+            label8.Location = new Point(297, 174);
             label8.Name = "label8";
             label8.Size = new Size(217, 25);
             label8.TabIndex = 13;
@@ -60,93 +61,70 @@
             // label14
             // 
             label14.AutoSize = true;
-            label14.Location = new Point(306, 150);
+            label14.Location = new Point(305, 45);
             label14.Name = "label14";
             label14.Size = new Size(116, 20);
             label14.TabIndex = 26;
             label14.Text = "Customer Name";
             // 
-            // dgItemList
+            // dgCustomerList
             // 
-            dgItemList.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            dgItemList.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgItemList.Columns.AddRange(new DataGridViewColumn[] { CusId, CusName, Gender, CusPhone });
-            dgItemList.Location = new Point(306, 360);
-            dgItemList.Name = "dgItemList";
-            dgItemList.RowHeadersWidth = 51;
-            dgItemList.Size = new Size(1164, 282);
-            dgItemList.TabIndex = 25;
-            // 
-            // CusId
-            // 
-            CusId.AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
-            CusId.HeaderText = "CusId";
-            CusId.MinimumWidth = 6;
-            CusId.Name = "CusId";
-            CusId.Width = 125;
-            // 
-            // CusName
-            // 
-            CusName.HeaderText = "CusName";
-            CusName.MinimumWidth = 6;
-            CusName.Name = "CusName";
-            // 
-            // Gender
-            // 
-            Gender.HeaderText = "Gender";
-            Gender.MinimumWidth = 6;
-            Gender.Name = "Gender";
-            // 
-            // CusPhone
-            // 
-            CusPhone.HeaderText = "Phone";
-            CusPhone.MinimumWidth = 6;
-            CusPhone.Name = "CusPhone";
+            dgCustomerList.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgCustomerList.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgCustomerList.Location = new Point(297, 567);
+            dgCustomerList.Name = "dgCustomerList";
+            dgCustomerList.RowHeadersWidth = 51;
+            dgCustomerList.Size = new Size(1164, 282);
+            dgCustomerList.TabIndex = 25;
+            dgCustomerList.Click += dgCustomerList_Click;
             // 
             // DeleteBtn
             // 
             DeleteBtn.BackColor = Color.Red;
             DeleteBtn.ForeColor = Color.White;
-            DeleteBtn.Location = new Point(970, 224);
+            DeleteBtn.Location = new Point(522, 120);
             DeleteBtn.Name = "DeleteBtn";
             DeleteBtn.Size = new Size(138, 41);
             DeleteBtn.TabIndex = 22;
-            DeleteBtn.Text = "Delete Item";
+            DeleteBtn.Text = "Delete Customer";
             DeleteBtn.UseVisualStyleBackColor = false;
+            DeleteBtn.Click += DeleteBtn_Click;
             // 
             // SaveBtn
             // 
             SaveBtn.BackColor = Color.FromArgb(0, 192, 0);
             SaveBtn.ForeColor = Color.White;
-            SaveBtn.Location = new Point(826, 224);
+            SaveBtn.Location = new Point(182, 120);
             SaveBtn.Name = "SaveBtn";
             SaveBtn.Size = new Size(138, 41);
             SaveBtn.TabIndex = 23;
             SaveBtn.Text = "Save";
             SaveBtn.UseVisualStyleBackColor = false;
+            SaveBtn.Click += SaveBtn_Click;
             // 
-            // EditBtn
+            // UpdateBtn
             // 
-            EditBtn.BackColor = Color.Yellow;
-            EditBtn.Location = new Point(682, 224);
-            EditBtn.Name = "EditBtn";
-            EditBtn.Size = new Size(138, 41);
-            EditBtn.TabIndex = 24;
-            EditBtn.Text = "Edit Item";
-            EditBtn.UseVisualStyleBackColor = false;
+            UpdateBtn.BackColor = Color.Yellow;
+            UpdateBtn.Location = new Point(351, 120);
+            UpdateBtn.Name = "UpdateBtn";
+            UpdateBtn.Size = new Size(138, 41);
+            UpdateBtn.TabIndex = 24;
+            UpdateBtn.Text = "Update";
+            UpdateBtn.UseVisualStyleBackColor = false;
+            UpdateBtn.Click += UpdateBtn_Click;
             // 
-            // cboCategory
+            // cboGender
             // 
-            cboCategory.FormattingEnabled = true;
-            cboCategory.Location = new Point(541, 173);
-            cboCategory.Name = "cboCategory";
-            cboCategory.Size = new Size(215, 28);
-            cboCategory.TabIndex = 21;
+            cboGender.FormattingEnabled = true;
+            cboGender.Location = new Point(590, 69);
+            cboGender.Name = "cboGender";
+            cboGender.Size = new Size(215, 28);
+            cboGender.TabIndex = 21;
             // 
             // label11
             // 
             label11.AutoSize = true;
-            label11.Location = new Point(781, 150);
+            label11.Location = new Point(861, 46);
             label11.Name = "label11";
             label11.Size = new Size(50, 20);
             label11.TabIndex = 18;
@@ -155,7 +133,7 @@
             // label10
             // 
             label10.AutoSize = true;
-            label10.Location = new Point(541, 150);
+            label10.Location = new Point(590, 47);
             label10.Name = "label10";
             label10.Size = new Size(57, 20);
             label10.TabIndex = 19;
@@ -165,48 +143,91 @@
             // 
             label9.AutoSize = true;
             label9.Font = new Font("Segoe UI", 12F);
-            label9.Location = new Point(306, 320);
+            label9.Location = new Point(297, 527);
             label9.Name = "label9";
             label9.Size = new Size(130, 28);
             label9.TabIndex = 20;
             label9.Text = "Customer List";
             // 
-            // txtPrice
+            // txtCusPhone
             // 
-            txtPrice.Location = new Point(781, 173);
-            txtPrice.Name = "txtPrice";
-            txtPrice.Size = new Size(216, 27);
-            txtPrice.TabIndex = 15;
+            txtCusPhone.Location = new Point(861, 68);
+            txtCusPhone.Name = "txtCusPhone";
+            txtCusPhone.Size = new Size(237, 27);
+            txtCusPhone.TabIndex = 15;
             // 
-            // txtItem
+            // txtCusName
             // 
-            txtItem.Location = new Point(306, 173);
-            txtItem.Name = "txtItem";
-            txtItem.Size = new Size(216, 27);
-            txtItem.TabIndex = 16;
+            txtCusName.Location = new Point(305, 68);
+            txtCusName.Name = "txtCusName";
+            txtCusName.Size = new Size(216, 27);
+            txtCusName.TabIndex = 16;
+            // 
+            // NewBtn
+            // 
+            NewBtn.BackColor = Color.DodgerBlue;
+            NewBtn.ForeColor = Color.White;
+            NewBtn.Location = new Point(14, 120);
+            NewBtn.Name = "NewBtn";
+            NewBtn.Size = new Size(138, 41);
+            NewBtn.TabIndex = 27;
+            NewBtn.Text = "New";
+            NewBtn.UseVisualStyleBackColor = false;
+            NewBtn.Click += NewBtn_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(txtCusID);
+            groupBox1.Controls.Add(NewBtn);
+            groupBox1.Controls.Add(DeleteBtn);
+            groupBox1.Controls.Add(label14);
+            groupBox1.Controls.Add(UpdateBtn);
+            groupBox1.Controls.Add(SaveBtn);
+            groupBox1.Controls.Add(txtCusName);
+            groupBox1.Controls.Add(label10);
+            groupBox1.Controls.Add(cboGender);
+            groupBox1.Controls.Add(label11);
+            groupBox1.Controls.Add(txtCusPhone);
+            groupBox1.Location = new Point(306, 217);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(1138, 235);
+            groupBox1.TabIndex = 28;
+            groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(14, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(91, 20);
+            label1.TabIndex = 29;
+            label1.Text = "Customer ID";
+            // 
+            // txtCusID
+            // 
+            txtCusID.Location = new Point(14, 69);
+            txtCusID.Name = "txtCusID";
+            txtCusID.Size = new Size(216, 27);
+            txtCusID.TabIndex = 28;
             // 
             // Customers
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1603, 751);
-            Controls.Add(label14);
-            Controls.Add(dgItemList);
-            Controls.Add(DeleteBtn);
-            Controls.Add(SaveBtn);
-            Controls.Add(EditBtn);
-            Controls.Add(cboCategory);
-            Controls.Add(label11);
-            Controls.Add(label10);
+            ClientSize = new Size(1924, 1054);
+            Controls.Add(dgCustomerList);
             Controls.Add(label9);
-            Controls.Add(txtPrice);
-            Controls.Add(txtItem);
             Controls.Add(label8);
+            Controls.Add(groupBox1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Customers";
             Text = "Customers";
             WindowState = FormWindowState.Maximized;
-            ((System.ComponentModel.ISupportInitialize)dgItemList).EndInit();
+            Load += Customers_Load;
+            ((System.ComponentModel.ISupportInitialize)dgCustomerList).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -215,19 +236,19 @@
 
         private Label label8;
         private Label label14;
-        private DataGridView dgItemList;
-        private DataGridViewTextBoxColumn CusId;
-        private DataGridViewTextBoxColumn CusName;
-        private DataGridViewTextBoxColumn Gender;
-        private DataGridViewTextBoxColumn CusPhone;
+        private DataGridView dgCustomerList;
         private Button DeleteBtn;
         private Button SaveBtn;
-        private Button EditBtn;
-        private ComboBox cboCategory;
+        private Button UpdateBtn;
+        private ComboBox cboGender;
         private Label label11;
         private Label label10;
         private Label label9;
-        private MaskedTextBox txtPrice;
-        private MaskedTextBox txtItem;
+        private MaskedTextBox txtCusPhone;
+        private MaskedTextBox txtCusName;
+        private Button NewBtn;
+        private GroupBox groupBox1;
+        private Label label1;
+        private MaskedTextBox txtCusID;
     }
 }
