ceb8a7fb8e8a702a5cf00fb5e86ec703cf59539f
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2024-12-04 16:31:27 +0630

    Add - Redesign Items and CRUD

--- PGDCAProject.InventoryManagementSystem/Items.Designer.cs
+++ PGDCAProject.InventoryManagementSystem/Items.Designer.cs
@@ -43,15 +43,11 @@
             SaveBtn = new Button();
             DeleteBtn = new Button();
             dgItemList = new DataGridView();
-            ItemId = new DataGridViewTextBoxColumn();
-            IName = new DataGridViewTextBoxColumn();
-            Category = new DataGridViewTextBoxColumn();
-            IPrice = new DataGridViewTextBoxColumn();
-            IStock = new DataGridViewTextBoxColumn();
-            IManufacutrer = new DataGridViewTextBoxColumn();
             label14 = new Label();
             groupBox1 = new GroupBox();
-            button1 = new Button();
+            label1 = new Label();
+            NewBtn = new Button();
+            txtItemId = new MaskedTextBox();
             ((System.ComponentModel.ISupportInitialize)dgItemList).BeginInit();
             groupBox1.SuspendLayout();
             SuspendLayout();
@@ -68,7 +64,7 @@
             // 
             // txtItem
             // 
-            txtItem.Location = new Point(340, 243);
+            txtItem.Location = new Point(282, 48);
             txtItem.Name = "txtItem";
             txtItem.Size = new Size(216, 26);
             txtItem.TabIndex = 4;
@@ -77,7 +73,7 @@
             // 
             label9.AutoSize = true;
             label9.Font = new Font("Microsoft Sans Serif", 12F);
-            label9.Location = new Point(300, 411);
+            label9.Location = new Point(300, 447);
             label9.Name = "label9";
             label9.Size = new Size(84, 25);
             label9.TabIndex = 5;
@@ -86,7 +82,7 @@
             // label10
             // 
             label10.AutoSize = true;
-            label10.Location = new Point(575, 220);
+            label10.Location = new Point(517, 25);
             label10.Name = "label10";
             label10.Size = new Size(76, 20);
             label10.TabIndex = 5;
@@ -94,7 +90,7 @@
             // 
             // txtPrice
             // 
-            txtPrice.Location = new Point(815, 243);
+            txtPrice.Location = new Point(757, 48);
             txtPrice.Name = "txtPrice";
             txtPrice.Size = new Size(216, 26);
             txtPrice.TabIndex = 4;
@@ -102,7 +98,7 @@
             // label11
             // 
             label11.AutoSize = true;
-            label11.Location = new Point(815, 220);
+            label11.Location = new Point(757, 25);
             label11.Name = "label11";
             label11.Size = new Size(48, 20);
             label11.TabIndex = 5;
@@ -110,7 +106,7 @@
             // 
             // txtStock
             // 
-            txtStock.Location = new Point(1053, 243);
+            txtStock.Location = new Point(995, 48);
             txtStock.Name = "txtStock";
             txtStock.Size = new Size(216, 26);
             txtStock.TabIndex = 4;
@@ -118,7 +114,7 @@
             // label12
             // 
             label12.AutoSize = true;
-            label12.Location = new Point(1053, 220);
+            label12.Location = new Point(995, 25);
             label12.Name = "label12";
             label12.Size = new Size(51, 20);
             label12.TabIndex = 5;
@@ -126,7 +122,7 @@
             // 
             // txtManufacturer
             // 
-            txtManufacturer.Location = new Point(1288, 243);
+            txtManufacturer.Location = new Point(1230, 48);
             txtManufacturer.Name = "txtManufacturer";
             txtManufacturer.Size = new Size(216, 26);
             txtManufacturer.TabIndex = 4;
@@ -134,7 +130,7 @@
             // label13
             // 
             label13.AutoSize = true;
-            label13.Location = new Point(1288, 220);
+            label13.Location = new Point(1230, 25);
             label13.Name = "label13";
             label13.Size = new Size(108, 20);
             label13.TabIndex = 5;
@@ -143,7 +139,7 @@
             // cboCategory
             // 
             cboCategory.FormattingEnabled = true;
-            cboCategory.Location = new Point(575, 243);
+            cboCategory.Location = new Point(517, 48);
             cboCategory.Name = "cboCategory";
             cboCategory.Size = new Size(215, 28);
             cboCategory.TabIndex = 6;
@@ -151,88 +147,53 @@
             // UpdateBtn
             // 
             UpdateBtn.BackColor = Color.Yellow;
-            UpdateBtn.Location = new Point(646, 97);
+            UpdateBtn.Location = new Point(757, 103);
             UpdateBtn.Name = "UpdateBtn";
             UpdateBtn.Size = new Size(138, 41);
             UpdateBtn.TabIndex = 7;
             UpdateBtn.Text = "Update";
             UpdateBtn.UseVisualStyleBackColor = false;
+            UpdateBtn.Click += UpdateBtn_Click;
             // 
             // SaveBtn
             // 
             SaveBtn.BackColor = Color.FromArgb(0, 192, 0);
             SaveBtn.ForeColor = Color.White;
-            SaveBtn.Location = new Point(481, 97);
+            SaveBtn.Location = new Point(592, 103);
             SaveBtn.Name = "SaveBtn";
             SaveBtn.Size = new Size(138, 41);
             SaveBtn.TabIndex = 7;
             SaveBtn.Text = "Save";
             SaveBtn.UseVisualStyleBackColor = false;
+            SaveBtn.Click += SaveBtn_Click;
             // 
             // DeleteBtn
             // 
             DeleteBtn.BackColor = Color.Red;
             DeleteBtn.ForeColor = Color.White;
-            DeleteBtn.Location = new Point(815, 97);
+            DeleteBtn.Location = new Point(926, 103);
             DeleteBtn.Name = "DeleteBtn";
             DeleteBtn.Size = new Size(138, 41);
             DeleteBtn.TabIndex = 7;
             DeleteBtn.Text = "Delete Item";
             DeleteBtn.UseVisualStyleBackColor = false;
+            DeleteBtn.Click += DeleteBtn_Click;
             // 
             // dgItemList
             // 
             dgItemList.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dgItemList.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgItemList.Columns.AddRange(new DataGridViewColumn[] { ItemId, IName, Category, IPrice, IStock, IManufacutrer });
-            dgItemList.Location = new Point(302, 442);
+            dgItemList.Location = new Point(302, 478);
             dgItemList.Name = "dgItemList";
             dgItemList.RowHeadersWidth = 51;
-            dgItemList.Size = new Size(1288, 539);
+            dgItemList.Size = new Size(1288, 506);
             dgItemList.TabIndex = 8;
-            // 
-            // ItemId
-            // 
-            ItemId.AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
-            ItemId.HeaderText = "ItemId";
-            ItemId.MinimumWidth = 6;
-            ItemId.Name = "ItemId";
-            ItemId.Width = 125;
-            // 
-            // IName
-            // 
-            IName.HeaderText = "ItemName";
-            IName.MinimumWidth = 6;
-            IName.Name = "IName";
-            // 
-            // Category
-            // 
-            Category.HeaderText = "Category";
-            Category.MinimumWidth = 6;
-            Category.Name = "Category";
-            // 
-            // IPrice
-            // 
-            IPrice.HeaderText = "ItemPrice";
-            IPrice.MinimumWidth = 6;
-            IPrice.Name = "IPrice";
-            // 
-            // IStock
-            // 
-            IStock.HeaderText = "ItemStock";
-            IStock.MinimumWidth = 6;
-            IStock.Name = "IStock";
-            // 
-            // IManufacutrer
-            // 
-            IManufacutrer.HeaderText = "ItemManufacutrer";
-            IManufacutrer.MinimumWidth = 6;
-            IManufacutrer.Name = "IManufacutrer";
+            dgItemList.Click += dgItemList_Click;
             // 
             // label14
             // 
             label14.AutoSize = true;
-            label14.Location = new Point(340, 220);
+            label14.Location = new Point(282, 25);
             label14.Name = "label14";
             label14.Size = new Size(41, 20);
             label14.TabIndex = 9;
@@ -240,44 +201,63 @@
             // 
             // groupBox1
             // 
-            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(NewBtn);
+            groupBox1.Controls.Add(txtItemId);
             groupBox1.Controls.Add(SaveBtn);
+            groupBox1.Controls.Add(label14);
             groupBox1.Controls.Add(UpdateBtn);
             groupBox1.Controls.Add(DeleteBtn);
+            groupBox1.Controls.Add(cboCategory);
+            groupBox1.Controls.Add(txtStock);
+            groupBox1.Controls.Add(label13);
+            groupBox1.Controls.Add(txtItem);
+            groupBox1.Controls.Add(label12);
+            groupBox1.Controls.Add(txtPrice);
+            groupBox1.Controls.Add(label11);
+            groupBox1.Controls.Add(txtManufacturer);
+            groupBox1.Controls.Add(label10);
             groupBox1.Location = new Point(302, 197);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(1241, 162);
+            groupBox1.Size = new Size(1477, 162);
             groupBox1.TabIndex = 10;
             groupBox1.TabStop = false;
             // 
-            // button1
+            // label1
             // 
-            button1.BackColor = Color.DodgerBlue;
-            button1.ForeColor = Color.White;
-            button1.Location = new Point(319, 97);
-            button1.Name = "button1";
-            button1.Size = new Size(138, 41);
-            button1.TabIndex = 8;
-            button1.Text = "New";
-            button1.UseVisualStyleBackColor = false;
+            label1.AutoSize = true;
+            label1.Location = new Point(41, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(63, 20);
+            label1.TabIndex = 12;
+            label1.Text = "Item ID";
+            // 
+            // NewBtn
+            // 
+            NewBtn.BackColor = Color.DodgerBlue;
+            NewBtn.ForeColor = Color.White;
+            NewBtn.Location = new Point(430, 103);
+            NewBtn.Name = "NewBtn";
+            NewBtn.Size = new Size(138, 41);
+            NewBtn.TabIndex = 8;
+            NewBtn.Text = "New";
+            NewBtn.UseVisualStyleBackColor = false;
+            NewBtn.Click += NewBtn_Click;
+            // 
+            // txtItemId
+            // 
+            txtItemId.Location = new Point(41, 50);
+            txtItemId.Name = "txtItemId";
+            txtItemId.Size = new Size(216, 26);
+            txtItemId.TabIndex = 11;
             // 
             // Items
             // 
             AutoScaleDimensions = new SizeF(10F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1924, 1054);
-            Controls.Add(label14);
             Controls.Add(dgItemList);
-            Controls.Add(cboCategory);
-            Controls.Add(label13);
-            Controls.Add(label12);
-            Controls.Add(label11);
-            Controls.Add(label10);
             Controls.Add(label9);
-            Controls.Add(txtManufacturer);
-            Controls.Add(txtStock);
-            Controls.Add(txtPrice);
-            Controls.Add(txtItem);
             Controls.Add(label8);
             Controls.Add(groupBox1);
             Font = new Font("Microsoft Sans Serif", 10F);
@@ -286,8 +266,10 @@
             Name = "Items";
             Text = "Items";
             WindowState = FormWindowState.Maximized;
+            Load += Items_Load;
             ((System.ComponentModel.ISupportInitialize)dgItemList).EndInit();
             groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -310,12 +292,8 @@
         private DataGridView dgItemList;
         private Label label14;
         private GroupBox groupBox1;
-        private Button button1;
-        private DataGridViewTextBoxColumn ItemId;
-        private DataGridViewTextBoxColumn IName;
-        private DataGridViewTextBoxColumn Category;
-        private DataGridViewTextBoxColumn IPrice;
-        private DataGridViewTextBoxColumn IStock;
-        private DataGridViewTextBoxColumn IManufacutrer;
+        private Button NewBtn;
+        private Label label1;
+        private MaskedTextBox txtItemId;
     }
 }
--- PGDCAProject.InventoryManagementSystem/Items.cs
+++ PGDCAProject.InventoryManagementSystem/Items.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+//using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Azure;
+using Microsoft.Data.SqlClient;
 
 namespace PGDCAProject.InventoryManagementSystem
 {
@@ -17,9 +20,155 @@ namespace PGDCAProject.InventoryManagementSystem
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+
+        SqlConnection consql;
+        string str;
+        DataSet Dset;
+
+        void Connection()
         {
-            this.Close();
+            string str = "Data Source=DESKTOP-L3SMK21\\SQLEXPRESS;Initial Catalog=ElectronicsDb;Persist Security Info=True;User ID=sa;Password=sasa@123;Trust Server Certificate=True";
+            consql = new SqlConnection(str);
+            consql.Open();
+        }
+
+        private void Clear()
+        {
+            txtItemId.Text = "";
+            txtItem.Text = "";
+            cboCategory.Text = "";
+            txtPrice.Text = "";
+            txtStock.Text = "";
+            txtManufacturer.Text = "";
+            txtItem.Focus();
+        }
+
+        private void FillCategoryName()
+        {
+            string query = "SELECT CategoryId, CategoryName FROM TblCategories";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, consql);
+            DataSet dataSet = new DataSet();
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataSet, "categories");
+            dataTable = dataSet.Tables["categories"]!;
+            cboCategory.DataSource = dataTable;
+            cboCategory.DisplayMember = dataTable.Columns["CategoryName"]!.ToString();
+            cboCategory.ValueMember = dataTable.Columns["CategoryId"]!.ToString();
+        }
+
+        private void AutoId()
+        {
+            string IName;
+            int IID;
+            string query = "SELECT ItemId FROM TblItems ORDER BY ItemId";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, consql);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "items");
+
+            if (ds.Tables["items"]!.Rows.Count > 0)
+            {
+                IName = ds.Tables["items"]!.Rows[^1][0].ToString()!;
+                IID = int.Parse(IName.Substring(1, (IName.Length - 1)));
+                txtItemId.Text = "S" + ((IID + 1).ToString("0000"));
+            }
+            else
+            {
+                txtItemId.Text = "S0001";
+            }
+        }
+
+        private void FillData()
+        {
+            string query = "SELECT ItemId, ItemName, TblCategories.CategoryName, ItemPrice, ItemStock, ItemManufacturer " +
+                "FROM TblItems INNER JOIN TblCategories ON TblItems.CategoryId = TblCategories.CategoryId;";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, consql);
+            Dset = new DataSet();
+            adapter.Fill(Dset, "items");
+
+            dgItemList.DataSource = Dset.Tables["items"];
+            dgItemList.ColumnHeadersDefaultCellStyle.Font = new Font("Tahoma", 9.75F, FontStyle.Bold);
+        }
+
+        private void Items_Load(object sender, EventArgs e)
+        {
+            Connection();
+            Clear();
+            FillCategoryName();
+            FillData();
+        }
+
+        private void NewBtn_Click(object sender, EventArgs e)
+        {
+            Clear();
+            AutoId();
+        }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "INSERT INTO TblItems VALUES" +
+                        "('" + txtItemId.Text + "', '" + txtItem.Text + "', '" + cboCategory.SelectedValue + "'," +
+                        "'" + int.Parse(txtPrice.Text) + "', '" + int.Parse(txtStock.Text) + "', '" + txtManufacturer.Text + "')";
+                SqlCommand cmd = new SqlCommand(query, consql);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Save new item is successful.", "Informartion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Clear();
+                FillData();
+            }
+            catch
+            {
+                MessageBox.Show("Something wrong!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dgItemList_Click(object sender, EventArgs e)
+        {
+            int i;
+            i = dgItemList.CurrentRow.Index;
+            txtItemId.Text = Dset.Tables["items"]!.Rows[i][0].ToString();
+            txtItem.Text = Dset.Tables["items"]!.Rows[i][1].ToString();
+            cboCategory.Text = Dset.Tables["items"]!.Rows[i][2].ToString();
+            txtPrice.Text = Dset.Tables["items"]!.Rows[i][3].ToString();
+            txtStock.Text = Dset.Tables["items"]!.Rows[i][4].ToString();
+            txtManufacturer.Text = Dset.Tables["items"]!.Rows[i][5].ToString();
+        }
+
+        private void UpdateBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "UPDATE TblItems SET ItemName = '" + txtItem.Text + "', CategoryId = '" + cboCategory.SelectedValue + "', " +
+                "ItemPrice = '" + txtPrice.Text + "', ItemStock = '" + txtStock.Text + "', ItemManufacturer = '" + txtManufacturer.Text + "'" +
+                "WHERE ItemId = '" + txtItemId.Text + "'";
+                SqlCommand cmd = new SqlCommand(query, consql);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Update item is successful.", "Informartion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Clear();
+                FillData();
+            }
+            catch
+            {
+                MessageBox.Show("Something wrong!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "DELETE FROM TblItems WHERE ItemId = '"+txtItemId.Text+"'";
+                SqlCommand cmd = new SqlCommand(query, consql);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Deleting item is successfull.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Clear();
+                FillData();
+            }
+            catch
+            {
+                MessageBox.Show("Something wrong!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
